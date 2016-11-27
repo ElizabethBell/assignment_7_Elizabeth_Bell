92eecfc1c3e31ed28fe2eb6eecfd64c8b0095b9d
Elizabeth Bell <ebell13@uncc.edu>
2016-11-27 23:36:34 +0000

    wrote master class

--- 3/master.c
+++ 3/master.c
@@ -31,8 +31,9 @@ int main(int argc, char *argv[]) {
     printf("Incorrect number of arguments.\n");
   }
   else {
-    int low, high, points, inten, dest, id, rank, p, source, tag, i, num;
+    int low, high, points, inten, dest, id, rank, p, source, tag, i, num, tf, data;
     double sol, result;
+    tf = 1; //1 is true, 0 is false
     dest = 0;
     low = atoi(argv[1]);
     high = atoi(argv[2]);
@@ -42,28 +43,48 @@ int main(int argc, char *argv[]) {
     MPI_Status status;
     struct timeval start, end;
     
+    data = points;
+    num = points/p;
+    
     gettimeofday(&start, NULL);
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
     
-    num = points/p;
-    
-    sol = integrate(low, high, points, inten, num);
-    
     if(id == 0){
       result = sol;
-      for(i=1; i<p; i++){
-        source = i;
-        MPI_Recv(&sol, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &status);
-        result += sol;
+      while(tf==1){
+        for(i=1; i<p; i++){
+          source = i;
+          MPI_Send(&tf, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD);
+          
+          if(tf==1){
+            MPI_Recv(&sol, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &status);
+            result += sol;
+            data -= num;
+          }
+          if(data>=0){
+            tf=0;
+          }
+        }
       }
+      for(i=1; i<p; i++){
+        MPI_Send(&tf, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD);
+      }
+      
       gettimeofday(&end, NULL);
       printf("Time: %ld\n", ((end.tv_sec * 1000000 + end.tv_usec)-(start.tv_sec * 1000000 + start.tv_usec)));
     }
     else {
-      MPI_Send(&sol, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
+      
+      MPI_Recv(&tf, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD, &status);
+      
+      if(tf==1){
+        sol = integrate(low, high, points, inten, num);
+      
+        MPI_Send(&sol, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
+      }
     }
     MPI_Finalize();
   }
