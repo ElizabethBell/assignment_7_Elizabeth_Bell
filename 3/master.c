da7c9d102e4728a551d6ba2faeb7ccb0ab3dcdab
Elizabeth Bell <ebell13@uncc.edu>
2016-11-27 22:15:05 +0000

    check

--- 3/master.c
+++ 3/master.c
@@ -15,11 +15,11 @@ double f (double n, int x) {
   return y;
 }
 
-double integrate (int a, int b, int n, int x) {
+double integrate (int a, int b, int n, int x, int num) {
   int i;
   double sol;
   
-  for (i=0; i<n; i++) {
+  for (i=0; i<num; i++) {
     double num = a+(i+0.5)*((b-a)/n);
     sol += f(num, x)*((b-a)/n);
   }
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
     printf("Incorrect number of arguments.\n");
   }
   else {
-    int low, high, points, inten, dest, id, rank, p, source, tag, i;
+    int low, high, points, inten, dest, id, rank, p, source, tag, i, num;
     double sol, result;
     dest = 0;
     low = atoi(argv[1]);
@@ -48,24 +48,26 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
     
-    sol = integrate(low, high, points, inten);
+    num = points/p;
     
-    if(id = 0){
+    sol = integrate(low, high, points, inten, num);
+    
+    if(id == 0){
       result = sol;
       for(i=1; i<p; i++){
         source = i;
         MPI_Recv(&sol, 1, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &status);
         result += sol;
       }
+      gettimeofday(&end, NULL);
+      printf("Time: %ld\n", ((end.tv_sec * 1000000 + end.tv_usec)-(start.tv_sec * 1000000 + start.tv_usec)));
     }
     else {
-      MPI_Send(&sol, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD, &status);
+      MPI_Send(&sol, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
     }
-    
     MPI_Finalize();
   }
-  gettimeofday(&end, NULL);
-  printf("Time: %ld\n", ((end.tv_sec * 1000000 + end.tv_usec)-(start.tv_sec * 1000000 + start.tv_usec)));
+  
   
   return 0;
 }
